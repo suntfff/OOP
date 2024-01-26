6bb987660cf26f3ed88ac2d6facd612a6a97df48
suntff <157761880+suntfff@users.noreply.github.com>
2024-01-26 22:49:27 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	class sphere {
+		int r;
+	public:
+		sphere (int(r)) {
+			if (r > 0) {
+				this->r = r;
+			}
+			else {
+				std::cout << "Значение должно быть больше нуля" << std::endl;
+			}
+		}
+		double volume() {
+			return (4 * acos(-1) * pow(this->r, 3))/3;
+		}
+		double square() {
+			return 4 * acos(-1) * pow(this->r, 2);
+		}
+
+	};
+	double r;
+	std::cout << "Введите значение радиуса:"; std::cin >> r; std::cout << std::endl;
+	sphere a(r);
+	std::cout<<"ОбЪем:"<<a.volume() << std::endl <<"Площадь поверхности:"<< a.square();
+}
